3af1250b5be05f8384897429318230d6acc49a3a
Code Sandbox <hello@codesandbox.io>
2022-02-25 18:51:01 +0000

    Deployment from Codesandbox.

--- _nuxt/static/1645813659/post/fashionable-textured-design@STUST-101/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1645813659",layout:"default",error:a,serverRendered:true,routePath:"\u002Fpost\u002Ffashionable-textured-design@STUST-101",config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},http:{browserBaseURL:b},content:{dbHash:"e4d52804"}}}}(null,"\u002F"));
