88411602c82d98bff7747782912a3437c4c9afee
Code Sandbox <hello@codesandbox.io>
2022-02-25 18:27:41 +0000

    Deployment from Codesandbox.

--- _nuxt/static/1645813196/post/SwissMax/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1645813196",layout:"default",error:a,serverRendered:true,routePath:"\u002Fpost\u002FSwissMax",config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},http:{browserBaseURL:b},content:{dbHash:"ef168f68"}}}}(null,"\u002F"));
