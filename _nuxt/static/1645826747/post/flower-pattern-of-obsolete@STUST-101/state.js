11391f6ba2899fb6303a71bbb8dc81b92c1f0a6e
Code Sandbox <hello@codesandbox.io>
2022-02-25 22:54:31 +0000

    Deployment from Codesandbox.

--- _nuxt/static/1645826747/post/flower-pattern-of-obsolete@STUST-101/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1645826747",layout:"default",error:a,serverRendered:true,routePath:"\u002Fpost\u002Fflower-pattern-of-obsolete@STUST-101",config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},http:{browserBaseURL:b},content:{dbHash:"a467891b"}}}}(null,"\u002F"));
---
+++ _nuxt/static/1645829669/post/motex-mask-design-competition@STUST-101/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1645829669",layout:"default",error:a,serverRendered:true,routePath:"\u002Fpost\u002Fmotex-mask-design-competition@STUST-101",config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},http:{browserBaseURL:b},content:{dbHash:"0c108d7e"}}}}(null,"\u002F"));
