808270cc20063ce752d5bd1ad069d545f16a3d58
Code Sandbox <hello@codesandbox.io>
2022-02-25 19:27:09 +0000

    Deployment from Codesandbox.

---
+++ _nuxt/static/1645817224/post/flower-pattern-of-obsolete@STUST-101/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1645817224",layout:"default",error:a,serverRendered:true,routePath:"\u002Fpost\u002Fflower-pattern-of-obsolete@STUST-101",config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},http:{browserBaseURL:b},content:{dbHash:"f6838d96"}}}}(null,"\u002F"));
