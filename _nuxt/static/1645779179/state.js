3ea0197d0c0dea7e5bccd97d706cbec80a70015e
Code Sandbox <hello@codesandbox.io>
2022-02-25 08:53:44 +0000

    Deployment from Codesandbox.

---
+++ _nuxt/static/1645779179/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1645779179",layout:"default",error:b,serverRendered:true,routePath:a,config:{_app:{basePath:a,assetsPath:"\u002F_nuxt\u002F",cdnURL:b},http:{browserBaseURL:a},content:{dbHash:"541f42ed"}}}}("\u002F",null));
