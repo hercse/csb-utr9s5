bc27d910545d1eabcdd739bc6711642cb3376ea6
Code Sandbox <hello@codesandbox.io>
2022-02-25 23:26:11 +0000

    Deployment from Codesandbox.

--- _nuxt/static/1645829985/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1645829985",layout:"default",error:b,serverRendered:true,routePath:a,config:{_app:{basePath:a,assetsPath:"\u002F_nuxt\u002F",cdnURL:b},http:{browserBaseURL:a},content:{dbHash:"a5d0bed7"}}}}("\u002F",null));
---
+++ _nuxt/static/1645831568/post/schooldex/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1645831568",layout:"default",error:a,serverRendered:true,routePath:"\u002Fpost\u002Fschooldex",config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},http:{browserBaseURL:b},content:{dbHash:"ad5cb5e3"}}}}(null,"\u002F"));
