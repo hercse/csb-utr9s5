59289aa090e38fa774b0d2e71d27a8e8892bf28d
Code Sandbox <hello@codesandbox.io>
2022-02-25 00:08:15 +0000

    Deployment from Codesandbox.

---
+++ _nuxt/static/1645747693/post/Koakinaimarche@2021-NADW/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1645747693",layout:"default",error:a,serverRendered:true,routePath:"\u002Fpost\u002FKoakinaimarche@2021-NADW",config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},http:{browserBaseURL:b},content:{dbHash:"9d495846"}}}}(null,"\u002F"));
