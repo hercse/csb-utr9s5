3826e24b43c08b6673976ecd4d530c940da803bc
Code Sandbox <hello@codesandbox.io>
2022-03-07 19:33:08 +0000

    Deployment from Codesandbox.

--- _nuxt/static/1646681443/post/motex-mask-design-competition@STUST-101/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1646681443",layout:"default",error:a,serverRendered:true,routePath:"\u002Fpost\u002Fmotex-mask-design-competition@STUST-101",config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},http:{browserBaseURL:b},content:{dbHash:"491a64f3"}}}}(null,"\u002F"));
