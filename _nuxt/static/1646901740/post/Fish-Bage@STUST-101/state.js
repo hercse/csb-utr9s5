54065bd757996b65c03744b806f964eca9371493
Code Sandbox <hello@codesandbox.io>
2022-03-10 08:48:46 +0000

    Deployment from Codesandbox.

--- _nuxt/static/1646901740/post/Fish-Bage@STUST-101/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1646901740",layout:"default",error:a,serverRendered:true,routePath:"\u002Fpost\u002FFish-Bage@STUST-101",config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},http:{browserBaseURL:b},content:{dbHash:"c28d1034"}}}}(null,"\u002F"));
---
+++ _nuxt/static/1646902122/archive/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1646902122",layout:"default",error:a,serverRendered:true,routePath:"\u002Farchive",config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},http:{browserBaseURL:b},content:{dbHash:"f42760d1"}}}}(null,"\u002F"));
